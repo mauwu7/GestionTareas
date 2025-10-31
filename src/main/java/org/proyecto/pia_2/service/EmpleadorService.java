0cad3207f1ded09740c2ba912602d508727f6b81
mauwu7 <mauricio45nove@gmail.com>
2025-10-30 23:04:13 -0600

    Interfaz para empleador

--- src/main/java/org/proyecto/pia_2/service/EmpleadorService.java
+++ src/main/java/org/proyecto/pia_2/service/EmpleadorService.java
@@ -1,4 +1,21 @@
 package org.proyecto.pia_2.service;
+import org.proyecto.pia_2.exception.EmpleadorNotFoundException;
+import org.proyecto.pia_2.exception.EntornoTrabajoException;
+import org.proyecto.pia_2.model.Empleador;
+import org.proyecto.pia_2.model.EntornoTrabajo;
+
+import java.util.List;
 
 public interface EmpleadorService {
+
+    Empleador agregarEntornoTrabajo(EntornoTrabajo entornoTrabajo, Long id) throws EntornoTrabajoException;
+
+    void agregarEmpleador(Empleador empleador);
+
+    Empleador GetEmpleador(Long id) throws EmpleadorNotFoundException;
+
+    Empleador EditarEmpleador(Empleador empleador, Long id) throws EmpleadorNotFoundException;
+
+    List<Empleador> obtenerEmpleadores();
+
 }
