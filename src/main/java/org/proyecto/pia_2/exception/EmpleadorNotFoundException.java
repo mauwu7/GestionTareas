09dfad8673abd23d8ff446de741c7ffdd7e79ad9
mauwu7 <mauricio45nove@gmail.com>
2025-10-30 23:08:17 -0600

    Excepciones para servicios

--- src/main/java/org/proyecto/pia_2/exception/EmpleadorNotFoundException.java
+++ src/main/java/org/proyecto/pia_2/exception/EmpleadorNotFoundException.java
@@ -1,7 +1,7 @@
 package org.proyecto.pia_2.exception;
 
 public class EmpleadorNotFoundException extends RuntimeException {
-  public EmpleadorNotFoundException(String message) {
-    super(message);
-  }
+    public EmpleadorNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/org/proyecto/pia_2/exception/EntornoTrabajoException.java
+++ src/main/java/org/proyecto/pia_2/exception/EntornoTrabajoException.java
@@ -1,7 +1,7 @@
-package org.proyecto.pia_2;
+package org.proyecto.pia_2.exception;
 
-public class EntornoTrabajoException extends RuntimeException {
-  public EntornoTrabajoException(String message) {
-    super(message);
-  }
+public class EntornoTrabajoException extends Exception {
+    public EntornoTrabajoException(String message) {
+        super(message);
+    }
 }
