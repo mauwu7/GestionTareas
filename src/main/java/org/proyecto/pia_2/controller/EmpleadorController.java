4d7aa7e9e575f3108e0d4d06dfb5b0d4b3d831d8
mauwu7 <mauricio45nove@gmail.com>
2025-10-31 22:11:09 -0600

    Agrego empleadorService a EmpleadorController

--- src/main/java/org/proyecto/pia_2/controller/EmpleadorController.java
+++ src/main/java/org/proyecto/pia_2/controller/EmpleadorController.java
@@ -1,7 +1,9 @@
 package org.proyecto.pia_2.controller;
+import jakarta.validation.Valid;
+import org.proyecto.pia_2.exception.UsuarioNotFoundException;
 import org.proyecto.pia_2.model.Empleador;
 import org.proyecto.pia_2.model.EntornoTrabajo;
-import org.proyecto.pia_2.repository.EmpleadorRepository;
+import org.proyecto.pia_2.service.impl.EmpleadorServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,46 +13,38 @@ import java.util.List;
 @RestController
 public class EmpleadorController {
 
-    private final EmpleadorRepository empleadorRepository;
+    private final EmpleadorServiceimpl empleadorService;
     @Autowired
-    public EmpleadorController(EmpleadorRepository empleadorRepository) {
-        this.empleadorRepository = empleadorRepository;
+    public EmpleadorController(EmpleadorServiceimpl empleadorService){
+        this.empleadorService = empleadorService;
     }
 
     @PostMapping("/addEmpleador")
-    ResponseEntity<Empleador> guardarEmpleador(@RequestBody Empleador empleador){
-        empleadorRepository.save(empleador);
+    ResponseEntity<Empleador> guardarEmpleador(@RequestBody @Valid Empleador empleador){
+        empleadorService.agregarEmpleador(empleador);
         return new ResponseEntity<Empleador>(empleador, HttpStatus.CREATED);
     }
 
     @PutMapping("/agregarEntornos/{idEmpleador}")
-    ResponseEntity<Empleador> agregarEntornosEmpleadores(@RequestBody EntornoTrabajo entornoTrabajo, @PathVariable Long idEmpleador){
-        Empleador empleador = empleadorRepository.findById(idEmpleador).orElse(null);
-        empleador.getEntornosDeTrabajo().add(entornoTrabajo); //Añadir Excepcion
-        empleadorRepository.save(empleador);
+    ResponseEntity<Empleador> agregarEntornosEmpleadores(@RequestBody @Valid EntornoTrabajo entornoTrabajo, @PathVariable Long idEmpleador) throws UsuarioNotFoundException{
+        Empleador empleador = empleadorService.agregarEntornoTrabajo(entornoTrabajo, idEmpleador);
         return new ResponseEntity<Empleador>(empleador,HttpStatus.OK);
     }
     @GetMapping("/requestEmpleador")
     public List<Empleador> obtenerEmpleadores(){
-        return empleadorRepository.findAll();
+        return empleadorService.obtenerEmpleadores();
     }
 
     @PutMapping("/editarEmpleador/{id}")
-    ResponseEntity<Empleador> EditarEmpleador(@RequestBody Empleador empleador, @PathVariable Long id){
-        Empleador empleadorEditado = empleadorRepository.findById(id).orElse(null);
-        empleadorEditado.setEmail(empleador.getEmail()); // AÑADIR EXCEPCION
-        empleadorEditado.setUsername(empleador.getUsername());
-        empleadorEditado.setPassword(empleador.getPassword());
-        empleadorRepository.save(empleadorEditado);
+    ResponseEntity<Empleador> EditarEmpleador(@RequestBody @Valid Empleador empleador, @PathVariable Long id) throws UsuarioNotFoundException {
+        Empleador empleadorEditado = empleadorService.EditarEmpleador(empleador, id);
         return new ResponseEntity<Empleador>(empleadorEditado,HttpStatus.OK);
     }
 
     @DeleteMapping("/eliminarEmpleador/{id}")
-    public void eliminarEmpleador(@PathVariable Long id){
-        empleadorRepository.deleteById(id);
+    public void eliminarEmpleador(@PathVariable Long id) throws UsuarioNotFoundException{
+        empleadorService.EliminarEmpleador(id);
     }
-
-
 
 
 
