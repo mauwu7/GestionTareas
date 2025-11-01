35c83d1580edd98d36a1a1fe056f6be19ce8abe3
mauwu7 <mauricio45nove@gmail.com>
2025-10-31 23:08:42 -0600

    Agrego clases para manejar las excepciones generadas

--- src/main/java/org/proyecto/pia_2/exception/model/ConstrViolation.java
+++ src/main/java/org/proyecto/pia_2/exception/model/ConstrViolation.java
@@ -1,4 +1,13 @@
 package org.proyecto.pia_2.exception.model;
 
 public class ConstrViolation {
+    private String field;
+    private String message;
+
+    public ConstrViolation(String field, String message)
+        {
+        this.field = field;
+        this.message = message;
+        }
+
 }
--- src/main/java/org/proyecto/pia_2/exception/model/ErrorResponse.java
+++ src/main/java/org/proyecto/pia_2/exception/model/ErrorResponse.java
@@ -1,4 +1,12 @@
 package org.proyecto.pia_2.exception.model;
 
 public class ErrorResponse {
+    private int statusCode;
+    private String message;
+
+    public ErrorResponse(String message, int statusCode) {
+        this.statusCode = statusCode;
+        this.message = message;
+
+    }
 }
