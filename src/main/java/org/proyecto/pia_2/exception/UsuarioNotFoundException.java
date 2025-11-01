bfd02914fc2c0c1f67a2a034a9f616dcdaa633ef
mauwu7 <mauricio45nove@gmail.com>
2025-10-31 22:13:52 -0600

    Agrego UsuarioNotFoundException

--- src/main/java/org/proyecto/pia_2/exception/UsuarioNotFoundException.java
+++ src/main/java/org/proyecto/pia_2/exception/UsuarioNotFoundException.java
@@ -1,7 +1,7 @@
 package org.proyecto.pia_2.exception;
 
-public class EmpleadorNotFoundException extends RuntimeException {
-    public EmpleadorNotFoundException(String message) {
+public class UsuarioNotFoundException extends Exception {
+    public UsuarioNotFoundException(String message) {
         super(message);
     }
 }
