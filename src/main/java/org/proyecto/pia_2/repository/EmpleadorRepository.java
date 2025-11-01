956cb4f817cd82e4d1805818de295d66e0f091bc
mauwu7 <mauricio45nove@gmail.com>
2025-10-31 23:14:32 -0600

    Nuevas consultas para el repositorio de Empleador

--- src/main/java/org/proyecto/pia_2/repository/EmpleadorRepository.java
+++ src/main/java/org/proyecto/pia_2/repository/EmpleadorRepository.java
@@ -1,9 +1,19 @@
 package org.proyecto.pia_2.repository;
 import org.proyecto.pia_2.model.Empleador;
+import org.proyecto.pia_2.model.EntornoTrabajo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmpleadorRepository extends JpaRepository<Empleador,Long> {
+
+    Boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+    Empleador findByEmail(String email);
     Empleador findByUsername(String username);
+    List<EntornoTrabajo> findEntornoTrabajos();
+
 }
