de3bd6011084f5afcae30fbd1cca1573820d107e
mauwu7 <mauricio45nove@gmail.com>
2025-11-01 23:28:18 -0600

    Se integra el test del servicio en el del controlador. Se renombra excepcion EntornoTrabajo

--- src/main/java/org/proyecto/pia_2/exception/EntornoTrabajoException.java
+++
@@ -1,7 +0,0 @@
-package org.proyecto.pia_2.exception;
-
-public class EntornoTrabajoException extends Exception {
-    public EntornoTrabajoException(String message) {
-        super(message);
-    }
-}
