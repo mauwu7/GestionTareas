967004440d6d19da8e2821f6d79fa8733503350a
mauwu7 <mauricio45nove@gmail.com>
2025-11-01 23:33:40 -0600

    Se añade consulta en el respositorio para servicio en empleador

--- src/main/java/org/proyecto/pia_2/repository/EntornoTrabajoRepository.java
+++ src/main/java/org/proyecto/pia_2/repository/EntornoTrabajoRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EntornoTrabajoRepository extends JpaRepository<EntornoTrabajo,Long> {
+    boolean existsByNombre(String nombre);
 }
