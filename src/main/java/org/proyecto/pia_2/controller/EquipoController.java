2549d460abcd0933d9ca97baccb56d3021a41835
semen power <semnupower.05@hotmail.com>
2025-11-01 23:30:54 -0600

    añadiendo funciones del controlador de equipos

--- src/main/java/org/proyecto/pia_2/controller/EquipoController.java
+++ src/main/java/org/proyecto/pia_2/controller/EquipoController.java
@@ -1,4 +1,50 @@
 package org.proyecto.pia_2.controller;
 
+import org.proyecto.pia_2.model.Empleado;
+import org.proyecto.pia_2.model.Equipo;
+import org.proyecto.pia_2.repository.EquipoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class EquipoController {
+    private final EquipoRepository equipoRepository;
+
+    @Autowired
+    public EquipoController(EquipoRepository equipoRepository){this.equipoRepository = equipoRepository;}
+
+    @PostMapping("/addEquipo")
+    public ResponseEntity<Equipo> addEquipo(@RequestBody Equipo equipo){
+        equipoRepository.save(equipo);
+        return new ResponseEntity<>(equipo, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deleteEquipo/{id}")
+    public void deleteEquipo(@PathVariable Long id){
+        equipoRepository.deleteById(id);
+    }
+
+    @GetMapping("/getEquipos")
+    public List<Equipo> getEquipos(){
+        return equipoRepository.findAll();
+    }
+
+    @PutMapping("/editarEquipo")
+    public ResponseEntity<Equipo> editEquipo(@RequestBody Equipo equipo, @PathVariable Long id){
+        Equipo equipoEdit = equipoRepository.findById(id).orElse(null);
+        if(equipoEdit == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        equipoEdit.setNombreEquipo(equipo.getNombreEquipo());
+        equipoEdit.setEntornoTrabajo(equipo.getEntornoTrabajo());
+        equipoEdit.setEmpleados(equipo.getEmpleados());
+        return new ResponseEntity<>(equipoRepository.save(equipoEdit), HttpStatus.OK);
+    }
+
+
 }
--- src/test/java/org/proyecto/pia_2/EmpleadoControllerTest.java
+++ src/test/java/org/proyecto/pia_2/EmpleadoControllerTest.java
@@ -1,11 +1,13 @@
 package org.proyecto.pia_2;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.proyecto.pia_2.controller.EmpleadoController;
+import org.proyecto.pia_2.model.Empleador;
 import org.proyecto.pia_2.repository.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,10 +17,12 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.proyecto.pia_2.model.Empleado;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import static org.mockito.ArgumentMatchers.any;
 
-import org.springframework.test.web.servlet.ResultActions;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(controllers = EmpleadoController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
+@JsonFormat(pattern = "yyyy-MM-dd")
 public class EmpleadoControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -34,12 +39,14 @@ public class EmpleadoControllerTest {
     @MockitoBean
     private EmpleadoRepository empleadoRepository;
 
+    private Empleado empleado;
+
     @Autowired
     private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setUp() {
-        Empleado empleado = new Empleado("manuel", "chele@example.com", "1234");
+        empleado = new Empleado("manuel", "chele@example.com", "1234");
         empleado.setUsuario_id(1L);
     }
 
@@ -65,11 +72,70 @@ public class EmpleadoControllerTest {
 
     @Test
     public void EmpleadoController_Eliminar() throws Exception {
+
         when(empleadoRepository.existsById(1L)).thenReturn(true);
         doNothing().when(empleadoRepository).deleteById(1L);
-        mockMvc.perform(MockMvcRequestBuilders.delete("/eliminarEmpleado/{id}",1L).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print());
-        verify(empleadoRepository, times(1)).deleteById(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/deleteEmpleado/{id}",1L).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print());
+
+        verify(empleadoRepository,times(1)).deleteById(1L);
     }
 
+    @Test
+    public void EmpleadoController_Actualizar() throws Exception {
+        String edit = """
+                {
+                       "username":"mauedit",
+                       "email":"putoEditado@gmail.com",
+                       "password":"1234edit"
+                }
+                """;
+        when(empleadoRepository.findById(1L)).thenReturn(Optional.of(empleado));
+        when(empleadoRepository.save(any(Empleado.class))).thenAnswer(i -> i.getArgument(0));
+        mockMvc.perform(MockMvcRequestBuilders.put("/editarEmpleado/{id}",1L).contentType(MediaType.APPLICATION_JSON)
+                        .content(edit)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andDo(print());
+        verify(empleadoRepository,times(1)).findById(1L);
+        verify(empleadoRepository,times(1)).save(any(Empleado.class));
 
+    }
+
+    @Test
+    public void EmpleadoController_AgregarTareasTest() throws Exception {
+        String jsonTarea = """
+                {
+                       "tarea_id":"1",
+                       "descripcion":"ejemplo",
+                       "fechaVencimiento":"2025-11-01",
+                       "fechaCreacion":"2025-10-31",
+                       "prioridad":"1",
+                       "estado":"PENDIENTE"
+                }
+                """;
+        when(empleadoRepository.findById(1L)).thenReturn(Optional.of(empleado));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/agregarTareas/{idEmpleado}",1L)
+                .contentType(MediaType.APPLICATION_JSON).content(jsonTarea).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print());
+
+        verify(empleadoRepository,times(1)).findById(1L);
+
+    }
+
+    @Test
+    public void EmpleadorController_GetTest() throws Exception {
+        Empleado empleado2 = new Empleado("mau", "mau@gmail.com", "15kilos");
+        empleado2.setUsuario_id(2L);
+
+        List<Empleado> empleados = Arrays.asList(empleado, empleado2);
+        when(empleadoRepository.findAll()).thenReturn(empleados);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/getEmpleados")
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print());
+
+        verify(empleadoRepository,times(1)).findAll();
+
+    }
 }
+
+
