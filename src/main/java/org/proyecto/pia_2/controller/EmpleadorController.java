ed2c731a1ed21442522c6f514da8c2bb300d165e
mauwu7 <mauricio45nove@gmail.com>
2025-11-04 18:40:50 -0600

    Añado endpoints para el empleador

--- src/main/java/org/proyecto/pia_2/controller/EmpleadorController.java
+++ src/main/java/org/proyecto/pia_2/controller/EmpleadorController.java
@@ -1,12 +1,14 @@
 package org.proyecto.pia_2.controller;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.proyecto.pia_2.exception.EquipoNotFoundException;
 import org.proyecto.pia_2.exception.EquipoRegistradoException;
 import org.proyecto.pia_2.exception.UsuarioNotFoundException;
 import org.proyecto.pia_2.exception.UsuarioRegistradoException;
-import org.proyecto.pia_2.model.Empleador;
-import org.proyecto.pia_2.model.EntornoTrabajo;
+import org.proyecto.pia_2.model.*;
 import org.proyecto.pia_2.service.impl.EmpleadorServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,17 +27,14 @@ public class EmpleadorController {
         this.empleadorService = empleadorService;
     }
 
-    @PostMapping("/addEmpleador")
+    //Operaciones generales sobre empleador-----------------------------------------------
+
+    @PostMapping("/addEmpleador") //Este metodo es momentaneo
     ResponseEntity<Empleador> guardarEmpleador(@RequestBody @Valid Empleador empleador) throws UsuarioRegistradoException{
-        return new ResponseEntity<Empleador>(empleadorService.agregarEmpleador(empleador), HttpStatus.CREATED);
+        return new ResponseEntity<>(empleadorService.agregarEmpleador(empleador), HttpStatus.CREATED);
     }
 
-    @PutMapping("/agregarEntornos/{idEmpleador}")
-    ResponseEntity<Empleador> agregarEntornosEmpleadores(@RequestBody @Valid EntornoTrabajo entornoTrabajo, @PathVariable @Min(1) @NotNull Long idEmpleador) throws EquipoRegistradoException, UsuarioNotFoundException{
-        Empleador empleador = empleadorService.agregarEntornoTrabajo(entornoTrabajo, idEmpleador);
-        return new ResponseEntity<Empleador>(empleador,HttpStatus.OK);
-    }
-    @GetMapping("/requestEmpleador")
+    @GetMapping("/requestEmpleador") //Este tambien
     public List<Empleador> obtenerEmpleadores(){
         return empleadorService.obtenerEmpleadores();
     }
@@ -43,7 +42,7 @@ public class EmpleadorController {
     @PutMapping("/editarEmpleador/{id}")
     ResponseEntity<Empleador> EditarEmpleador(@RequestBody @Valid Empleador empleador, @PathVariable @Min(1) @NotNull Long id) throws UsuarioNotFoundException, UsuarioRegistradoException {
         Empleador empleadorEditado = empleadorService.EditarEmpleador(empleador, id);
-        return new ResponseEntity<Empleador>(empleadorEditado,HttpStatus.OK);
+        return new ResponseEntity<>(empleadorEditado,HttpStatus.OK);
     }
 
     @DeleteMapping("/eliminarEmpleador/{id}")
@@ -51,6 +50,39 @@ public class EmpleadorController {
         empleadorService.EliminarEmpleador(id);
     }
 
+    //Operaciones en los entornos de Trabajo----------------------------------------
+    @PostMapping("/agregarEquipos/{nombreEntorno}")
+    ResponseEntity<EntornoTrabajo> agregarEquipoAEntornoDeTrabajo(@RequestBody @Valid Equipo equipoAgregado, @PathVariable @NotEmpty String nombreEntorno) throws EquipoRegistradoException {
+        return new ResponseEntity<> (empleadorService.AgregarEquipoEnEntornoDeTrabajo(equipoAgregado, nombreEntorno), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/agregarEntornos/{idEmpleador}")
+    ResponseEntity<Empleador> agregarEntornosEmpleadores(@RequestBody @Valid EntornoTrabajo entornoTrabajo, @PathVariable @Min(1) @NotNull Long idEmpleador) throws EquipoRegistradoException, UsuarioNotFoundException{
+        Empleador empleador = empleadorService.agregarEntornoTrabajo(entornoTrabajo, idEmpleador);
+        return new ResponseEntity<>(empleador,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/editarEntornodeTrabajo/{nombreEntorno}") //Considerar añadir el id del empleador que tiene asignado ese entorno de trabajo
+    ResponseEntity<EntornoTrabajo> EditarEntornoDeTrabajo(@RequestBody @Valid EntornoTrabajo entornoTrabajo, @PathVariable @NotBlank String nombreEntorno) throws EquipoNotFoundException, EquipoRegistradoException{
+        return new ResponseEntity<>(empleadorService.EditarEntorno(entornoTrabajo,nombreEntorno),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/eliminarEntornodeTrabajo/{nombreEntorno}")
+    void EliminarEntornoDeTrabajo(@PathVariable @NotBlank String nombreEntorno) throws UsuarioNotFoundException{
+        empleadorService.EliminarEntornoDeTrabajo(nombreEntorno);
+    }
+
+    //Operaciones en los Equipos---------------------------------------------------
+    @PostMapping("/agregarEmpleado/{nombreEquipo}")
+    ResponseEntity<Equipo> agregarEmpleadoEnEquipo(@RequestBody @Valid Empleado empleadoAgregado, @PathVariable @NotEmpty String nombreEquipo){
+        return new ResponseEntity<>(empleadorService.AgregarEmpleadoaEquipo(empleadoAgregado,nombreEquipo),HttpStatus.CREATED);
+    }
+
+    //Operaciones en los empleados y las tareas--------------------------------------------------
+    @PostMapping("/agregarTarea/{nombreEmpleado}")
+    ResponseEntity<Empleado> agregarTareaEnEmpleado(@RequestBody @Valid TareaIndividual tarea, @PathVariable @NotEmpty String nombreEmpleado) throws UsuarioNotFoundException{
+        return new ResponseEntity<>(empleadorService.AgregarTarea(tarea, nombreEmpleado),HttpStatus.CREATED);
+    }
 
 
 }
