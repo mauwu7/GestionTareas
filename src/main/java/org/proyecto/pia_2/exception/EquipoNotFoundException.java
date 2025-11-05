36be68c7998276e77c42bce3c986c59f5907309f
mauwu7 <mauricio45nove@gmail.com>
2025-11-04 18:16:40 -0600

    Agrego excepcion para servicio

---
+++ src/main/java/org/proyecto/pia_2/exception/EquipoNotFoundException.java
@@ -0,0 +1,7 @@
+package org.proyecto.pia_2.exception;
+
+public class EquipoNotFoundExceptin extends RuntimeException {
+  public EquipoNotFoundExceptin(String message) {
+    super(message);
+  }
+}
