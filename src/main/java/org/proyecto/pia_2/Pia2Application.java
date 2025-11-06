4d7eafae9ebc047f5d0ceaec723474dd5270e68e
mauwu7 <mauricio45nove@gmail.com>
2025-11-05 22:46:51 -0600

    .

--- src/main/java/org/proyecto/pia_2/Pia2Application.java
+++ src/main/java/org/proyecto/pia_2/Pia2Application.java
@@ -1,10 +1,6 @@
 package org.proyecto.pia_2;
-
-import org.proyecto.pia_2.model.Tarea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Queue;
 
 @SpringBootApplication
 public class Pia2Application {
@@ -13,7 +9,4 @@ public class Pia2Application {
         SpringApplication.run(Pia2Application.class, args);
     }
 
-    public static class GestorTareasUsuario {
-        Queue<Tarea> tareasPendientes;
-    }
 }
