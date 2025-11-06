bfd27313c416fbe0f9030d25bff29dda4521c7f4
mauwu7 <mauricio45nove@gmail.com>
2025-11-06 17:19:34 -0600

    Añado servicio para controlador Empleado

--- src/main/java/org/proyecto/pia_2/service/EmpleadoService.java
+++ src/main/java/org/proyecto/pia_2/service/EmpleadoService.java
@@ -1,4 +1,10 @@
 package org.proyecto.pia_2.service;
+import org.proyecto.pia_2.exception.TareaNotFoundException;
+import org.proyecto.pia_2.exception.UsuarioNotFoundException;
+import org.proyecto.pia_2.model.TareaIndividual;
+
+import java.util.List;
 
 public interface EmpleadoService {
+    List<TareaIndividual>  ConsultarTareas (Long idEmpleado) throws UsuarioNotFoundException, TareaNotFoundException;
 }
