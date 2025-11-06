b40fb54cfe0e7736a6629bc92713191ae500a028
mauwu7 <mauricio45nove@gmail.com>
2025-11-06 17:25:41 -0600

    Clae provisional para editar Tareas

--- src/main/java/org/proyecto/pia_2/DTO/TareaDTO.java
+++ src/main/java/org/proyecto/pia_2/DTO/TareaDTO.java
@@ -6,8 +6,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 public class TareaDTO {
 
-
-
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @NotNull
     private LocalDate fechaVencimiento;
