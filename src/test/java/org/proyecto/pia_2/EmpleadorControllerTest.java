8a87f72cfb2eb124599822a19fef5af437d1e267
mauwu7 <mauricio45nove@gmail.com>
2025-11-07 22:33:32 -0600

    Revisar los test

--- src/test/java/org/proyecto/pia_2/EmpleadorControllerTest.java
+++ src/test/java/org/proyecto/pia_2/EmpleadorControllerTest.java
@@ -19,9 +19,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.time.LocalDate;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
