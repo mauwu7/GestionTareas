dc1a20e7df987a48b76701f93af8601501fa282b
mauwu7 <mauricio45nove@gmail.com>
2025-11-09 22:40:33 -0600

    Agrego PATCH para modificaciones parciales

--- src/main/java/org/proyecto/pia_2/controller/EmpleadorController.java
+++ src/main/java/org/proyecto/pia_2/controller/EmpleadorController.java
@@ -1,17 +1,19 @@
 package org.proyecto.pia_2.controller;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.proyecto.pia_2.DTO.TareaDTO;
 import org.proyecto.pia_2.exception.*;
 import org.proyecto.pia_2.model.*;
 import org.proyecto.pia_2.service.impl.EmpleadorServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -27,13 +29,11 @@ public class EmpleadorController {
 
     //Operaciones generales sobre empleador-----------------------------------------------
 
-    //--check
     @PostMapping("/addEmpleador") //Este metodo es momentaneo
     ResponseEntity<Empleador> guardarEmpleador(@RequestBody @Valid Empleador empleador) throws UsuarioRegistradoException{
         return new ResponseEntity<>(empleadorService.agregarEmpleador(empleador), HttpStatus.CREATED);
     }
 
-    //-check
     @GetMapping("/requestEmpleador") //Este tambien
     public List<Empleador> obtenerEmpleadores(){
         return empleadorService.obtenerEmpleadores();
@@ -46,7 +46,6 @@ public class EmpleadorController {
         return new ResponseEntity<>(empleadorEditado,HttpStatus.OK);
     }
 
-    //-check
     @DeleteMapping("/eliminarEmpleador/{id}")
     public void eliminarEmpleador(@PathVariable @Min(1) @NotNull Long id) throws UsuarioNotFoundException{
         empleadorService.EliminarEmpleador(id);
@@ -54,65 +53,64 @@ public class EmpleadorController {
 
     //Operaciones en los entornos de Trabajo----------------------------------------
 
-    //--check
     @PostMapping("/agregarEquipos/{nombreEntorno}")
     ResponseEntity<EntornoTrabajo> agregarEquipoAEntornoDeTrabajo(@RequestBody @Valid Equipo equipoAgregado, @PathVariable @NotBlank String nombreEntorno) throws EquipoRegistradoException {
         return new ResponseEntity<> (empleadorService.AgregarEquipoEnEntornoDeTrabajo(equipoAgregado, nombreEntorno), HttpStatus.CREATED);
     }
 
-    //--check
     @PostMapping("/agregarEntornos/{idEmpleador}")
     ResponseEntity<Empleador> agregarEntornosEmpleadores(@RequestBody @Valid EntornoTrabajo entornoTrabajo, @PathVariable @Min(1) @NotNull Long idEmpleador) throws EquipoRegistradoException, UsuarioNotFoundException{
         Empleador empleador = empleadorService.agregarEntornoTrabajo(entornoTrabajo, idEmpleador);
         return new ResponseEntity<>(empleador,HttpStatus.CREATED);
     }
 
-    //--check
     @PutMapping("/editarEntornodeTrabajo/{nombreEntorno}") //Considerar añadir el id del empleador que tiene asignado ese entorno de trabajo
     ResponseEntity<EntornoTrabajo> EditarEntornoDeTrabajo(@RequestBody @Valid EntornoTrabajo entornoTrabajo, @PathVariable @NotBlank String nombreEntorno) throws EquipoNotFoundException, EquipoRegistradoException{
         return new ResponseEntity<>(empleadorService.EditarEntorno(entornoTrabajo,nombreEntorno),HttpStatus.OK);
     }
 
-    //--check
-    @DeleteMapping("/eliminarEntornodeTrabajo/{nombreEntorno}") //check
+    @DeleteMapping("/eliminarEntornodeTrabajo/{nombreEntorno}")
     void EliminarEntornoDeTrabajo(@PathVariable @NotBlank String nombreEntorno){
         empleadorService.EliminarEntornoDeTrabajo(nombreEntorno);
     }
 
     //Operaciones en los Equipos---------------------------------------------------
 
-    //--check
+
     @PostMapping("/agregarEmpleado/{nombreEquipo}")
     ResponseEntity<Equipo> agregarEmpleadoEnEquipo(@RequestBody @Valid Empleado empleadoAgregado, @PathVariable @NotBlank String nombreEquipo){
         return new ResponseEntity<>(empleadorService.AgregarEmpleadoaEquipo(empleadoAgregado,nombreEquipo),HttpStatus.CREATED);
     }
-    //--check
+
     @PutMapping("/editarEquipo/{nombreEquipo}")
     ResponseEntity<Equipo> EditarEquipo(@RequestBody @Valid Equipo equipoEditado, @PathVariable @NotBlank String nombreEquipo) throws EquipoRegistradoException, EquipoNotFoundException{
         return new ResponseEntity<>(empleadorService.EditarEquipo(equipoEditado, nombreEquipo),HttpStatus.OK);
     }
 
-    //--check
     @DeleteMapping("/eliminarEquipo/{nombreEquipo}")
     void EliminarEquipo(@PathVariable @NotBlank String nombreEquipo) throws EquipoNotFoundException{
         empleadorService.eliminarEquipo(nombreEquipo);
     }
 
     //Operaciones en los empleados y las tareas--------------------------------------------------
-    @PostMapping("/agregarTarea/{nombreEmpleado}")//---check
+    @PostMapping("/agregarTarea/{nombreEmpleado}")
     ResponseEntity<TareaIndividual> agregarTareaEnEmpleado(@RequestBody @Valid  TareaIndividual tarea, @PathVariable @NotBlank String nombreEmpleado) throws UsuarioNotFoundException{
         return new ResponseEntity<>(empleadorService.AgregarTarea(tarea, nombreEmpleado),HttpStatus.CREATED);
     }
 
-    //CORREGIR ESTO-------------------------------
-    @PutMapping("/editarTarea/{idTarea}/{nombreEmpleado}")
-    ResponseEntity<Empleado> editarTarea(@RequestBody @Valid TareaDTO tarea, @PathVariable @NotNull @Min(1) Long idTarea, @PathVariable @NotBlank String nombreEmpleado) throws UsuarioNotFoundException, TareaNotFoundException {
-        return new ResponseEntity<>(empleadorService.EditarTarea(tarea,idTarea, nombreEmpleado),HttpStatus.OK);
+    @PatchMapping("editarPrioridad/{idTarea}/{prioridad}/{nombreEmpleado}")
+    void editarTareaPrioridad(@Min(1) @Max(3) @NotNull @PathVariable Integer prioridad, @PathVariable @NotBlank String nombreEmpleado, @PathVariable @Min(1) @NotNull Long idTarea) throws UsuarioNotFoundException, TareaNotFoundException{
+        empleadorService.editarPrioridadTarea(idTarea, prioridad, nombreEmpleado);
     }
 
-    //----check
-    @DeleteMapping("/eliminarTarea/{idTarea}/{nombreEmpleado}")
-    void EliminarTarea(@PathVariable @NotNull @Min(1) Long idTarea, @PathVariable @NotBlank String nombreEmpleado) throws UsuarioNotFoundException, TareaNotFoundException{
-        empleadorService.eliminarTarea(idTarea, nombreEmpleado);
+    @PatchMapping("/editarFecha/{fechaVencimiento}/{idTarea}/{nombreEmpleado}")
+    void editarTareaFechaVencimiento(@NotNull @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable LocalDate fechaVencimiento, @Min(1) @NotNull @PathVariable Long idTarea,  @NotNull @PathVariable String nombreEmpleado) throws UsuarioNotFoundException, TareaNotFoundException{
+        empleadorService.editarfechaVencimiento(idTarea, fechaVencimiento, nombreEmpleado);
     }
+
+    @DeleteMapping("/finalizarTarea/{idTarea}/{nombreEmpleado}")
+    void eliminarTarea(@PathVariable @Min(1) @NotNull Long idTarea, @PathVariable @NotBlank String nombreEmpleado) throws UsuarioNotFoundException, TareaNotFoundException{
+        empleadorService.EliminarTarea(idTarea, nombreEmpleado);
+    }
+
 }
