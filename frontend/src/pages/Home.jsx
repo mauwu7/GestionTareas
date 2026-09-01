b01e0696a78307d8d526e5087ec94c450f01b2bf
mauwu7 <mauricio45nove@gmail.com>
2026-09-01 13:19:04 -0600

    dashboard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import styles from '../styles/Home.module.css'
+import { Icon } from '@mdi/react';
+import { mdiViewDashboard, mdiHome, mdiCardAccountDetailsOutline, mdiMessage, mdiNoteMultiple, mdiAccountGroup } from "@mdi/js";
 
 
 export default function Home ({ setToken }){
@@ -13,19 +15,31 @@ export default function Home ({ setToken }){
 
     } 
 
-    /**Cambiar orden etiquetas */
 
     return (
         <div className={styles.main}>
-            <header className={styles.header}>
-
-            </header>
             <nav className={styles.nav}>
-
+                <div className={styles.headerNav}>
+                    <Icon path={mdiViewDashboard} size={2} color='white'/>
+                    <h2>Dashboard</h2>
+                </div>
+                <section className={styles.navMenu}>
+                    <Icon path={mdiHome} size={1.2}/>
+                    <p>Home</p>
+                    <Icon path={mdiCardAccountDetailsOutline} size={1.2}/>
+                    <p>Profile</p>
+                    <Icon path={mdiMessage} size={1.2}/>
+                    <p>Messages</p>
+                    <Icon path={mdiNoteMultiple} size={1} />
+                    <p>Tasks</p>
+                    <Icon path={mdiAccountGroup} size={1} />
+                    <p>Grupos</p>
+                </section>
             </nav>
             <main className={styles.body}>
 
             </main>
+
         </div>
     )
 }
